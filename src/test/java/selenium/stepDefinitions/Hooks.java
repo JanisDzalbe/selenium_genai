fe4ab7d1d5d28e5cfb87fddabc70f59663c2fecb
Seva280701 <seva280701@gmail.com>
2026-04-27 15:23:03 +0300

    setup changes

--- src/test/java/selenium/stepDefinitions/Hooks.java
+++ src/test/java/selenium/stepDefinitions/Hooks.java
@@ -2,19 +2,22 @@ package selenium.stepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Hooks {
     public static WebDriver driver;
 
     @Before
-    public void openBrowser() throws MalformedURLException {
-        // TODO: set up browser based on OS and driver available in 'lib' directory
+    public void openBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @After
-    public void closeBrowser(Scenario scenario) {
-        driver.quit();
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
