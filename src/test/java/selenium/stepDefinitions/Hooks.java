7fe944c1bdf078c73102b67a40b2d1532fe1b5ee
Ilmārs Cīrulis <6750944+IlmarsCirulis@users.noreply.github.com>
2026-04-27 11:20:12 +0300

    update hooks and also Java version to 26

--- src/test/java/selenium/stepDefinitions/Hooks.java
+++ src/test/java/selenium/stepDefinitions/Hooks.java
@@ -5,6 +5,8 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.WebDriver;
 
+import java.net.MalformedURLException;
+
 public class Hooks {
     public static WebDriver driver;
 
