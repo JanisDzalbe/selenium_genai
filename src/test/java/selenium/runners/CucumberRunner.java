56c2d5f73cdf4c309aa4df52719392b7febf1b7a
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2026-04-27 22:36:53 +0300

    feat: add Cucumber test runner configuration

---
+++ src/test/java/selenium/runners/CucumberRunner.java
@@ -0,0 +1,15 @@
+package selenium.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        plugin = {"pretty", "html:target/cucumber-report.html"},
+        tags = "not @bug",
+        glue = {"selenium.stepDefinitions"}
+)
+public class CucumberRunner {
+}
